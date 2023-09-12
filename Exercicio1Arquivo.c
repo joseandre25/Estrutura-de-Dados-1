6d658da0fccae21dbc011dbd7adb84ae7b8880fe
joseandre25 <andrerabelo2503@gmail.com>
2023-09-12 20:34:49 -0300

    Test

---
+++ Exercicio1Arquivo.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main() {
+    FILE *fp;
+    char c;
+    int qtdChar = 0;
+    int qtdCharF;
+    int qtdEsp = 0;
+    int qtdTab = 0;
+    int qtdNewLines = 0;
+
+    fp = fopen("print.txt", "r");
+
+    if (fp == NULL) {
+        perror("Erro ao abrir o arquivo");
+        return 1;
+    }
+
+    while ((c = fgetc(fp)) != EOF) {
+        qtdChar++;
+
+        if (c == 32) {
+            qtdEsp++;
+        }
+        if (c == '\t') {
+            qtdTab++;
+        }
+        if (c == '\n') {
+            qtdNewLines++;
+        }
+    }
+
+    fclose(fp);
+    qtdCharF = qtdChar - (qtdEsp + qtdTab + qtdNewLines);
+
+    printf("Quantidade de Caracteres: %d\n", qtdCharF);
+    printf("Quantidade de Espaços: %d\n", qtdEsp);
+    printf("Quantidade de Tabs: %d\n", qtdTab);
+    printf("Quantidade de Newlines: %d\n", qtdNewLines);
+
+    return 0;
+}
