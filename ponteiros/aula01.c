7cebc0ace219b3176322d59fd9538a0f5537e904
joseandre25 <andrerabelo2503@gmail.com>
2023-10-03 10:27:42 -0300

    Aula 01 de ponteiros

---
+++ ponteiros/aula01.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int numero, *ptr, **ptr2;
+    numero = 79417;
+    ptr = &numero;
+    *ptr = 90560;
+    //ptr2 = &ptr;
+  
+    printf("Valor: %d\n", numero);
+    printf("Endereço: %p\n", &numero);
+    printf("Ponteiro: %p\n", ptr);
+    printf("Ponteiro pra ponteiro: %p\n", ptr2);
+
+    return 0;
+}
